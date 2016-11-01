208f94cb5d07833cf48125904aa1b49724e1c95d
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2016-11-01 18:25:09 -0300

    correccion error de sintaxis

--- public/js/app/controllers/viajes-controller.js
+++ public/js/app/controllers/viajes-controller.js
@@ -7,7 +7,7 @@ function ViajesController(){
 ViajesController.prototype = {
     load : function (){
       var navigationController = new NavigationController;
-      $.get('api/v1/viajes/',function(data){
+      $.get('api/v1/viajes',function(data){
         navigationController.loadTemplate('viajes',data,'#main-container',function(){
           navigationController.handleNavigationEvents();
         });
