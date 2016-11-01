8a688a3b371bd38fdf542ed4d07dac93e864bef9
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2016-11-01 18:25:26 -0300

    sintaxis

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ $app->get('{type:\w*}', [
 
 $app->get('api/v1/navigation[/{role}]', 'NavigationController@menu');
 //Testing
+$app->get('api/v1/viajes','ViajesController@getViajes');
 $app->get('api/v1/viajes/{id}', 'ViajesController@getViaje');
 $app->post('api/v1/viajes','ViajesController@addViaje');
-$app->get('api/v1/viajes','ViajesController@getViajes');
