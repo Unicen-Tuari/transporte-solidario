dba37a10d59d55e8e4f785ad697417ec8d196f0b
VirutaMaster <virubiru@hotmail.com>
2016-11-09 17:14:09 -0300

    se edito el controlador para la fucion Ver Historial de Viajes

--- app/Http/Controllers/ViajesController.php
+++ app/Http/Controllers/ViajesController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Models\viajeModel;
 
 class ViajesController extends Controller
 {
+
+    protected $model;
     /**
      * Create a new controller instance.
      *
@@ -13,6 +16,7 @@ class ViajesController extends Controller
      */
     public function __construct()
     {
+      $this->model = new ViajeModel;
         //
     }
 
@@ -25,6 +29,24 @@ class ViajesController extends Controller
       );
     }
 
+  public function getViajes(){
+          $var=new \stdClass;
+          $var->data=array(array(
+            'id' => 1,
+            'partida' => 'Tandil',
+            'destino' => 'Tandil',
+            'ONG' => 'Mesa Solidaria'),
+            array(
+            'id' => 1,
+            'partida' => 'Tandil',
+            'destino' => 'Tandil',
+            'ONG' => 'Mesa Solidaria')
+          );
+          return json_encode($var);
+
+        }
+
+
     public function addViaje(Request $request){
       $partida = $request->input('partida');
       //REPOSITORIO
@@ -32,6 +54,35 @@ class ViajesController extends Controller
         'id' => 'nuevo id',
         'partida' => $partida
       );
+    }
+
+    public function getHistory($id){
+      $var=new \stdClass;
+      $var->data=array(
+                      array(
+                            'id' => 14125,
+                            'fecha_inicio' => '02/05/2016',
+                            'fecha_final' => '02/05/2016',
+                            'colaborador' => 'Los Sauces S.A.',
+                            'destino' => 'Tandil',
+                            'partida' => 'Rauch',
+                            'tamanio' => '2 Plazas',
+                            'peso' => '22 kg',
+                            'distancia' => '129 km'
+                          ),
+                      array(
+                            'id' => 102938,
+                            'fecha_inicio' => '02/05/2016',
+                            'fecha_final' => '02/05/2016',
+                            'colaborador' => 'Juan Carlos Perez',
+                            'destino' => 'Mar del Plata',
+                            'partida' => 'Tandil',
+                            'tamanio' => '1 Plaza',
+                            'peso' => '7 kg',
+                            'distancia' => '277 km'
+                          )
+                      );
+      return json_encode($var);
     }
 
     //
