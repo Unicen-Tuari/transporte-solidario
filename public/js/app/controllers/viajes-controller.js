78d2b15581a092c225097d32bb7ac6be7bb7e545
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2016-11-19 16:17:38 -0300

    agregue funcionalidad para ordenar el listado de pantalla

--- public/js/app/controllers/viajes-controller.js
+++ public/js/app/controllers/viajes-controller.js
@@ -1,8 +1,7 @@
 
   "uses strict"
-
   function ViajesController(){}
-var viajes;
+  var viajes;
   ViajesController.prototype = {
       load : function (){
         var navigationController = new NavigationController;
@@ -32,29 +31,28 @@ var viajes;
                 navigationController.loadTemplate('listViajes',viajes,'#listadoViajes');
             });
 
-            /*
+
             $('#ordenarpor li a').click(function(e){
               e.preventDefault();
-              var filtro=$(this).attr("filtro");
-              var tipoFiltro=$(this).attr("tipoFilt");
-              var viajesFiltrados = (response.data.viajes).short(function(viaje,index){
-                switch(tipoFiltro){
-                  case "destino":return viaje.destino == filtro;
-                                 break;
-                  case "frecuencia":return viaje.frecuencia == filtro;
-                                    break;
-                  case "name":return viaje.name == filtro;
-                                 break;
+              var criterioOrden=$(this).attr("orden");
+              var viajesOrdenados = (response.data.viajes).sort(function(a,b){
+                switch(criterioOrden){
+                  case "peso":return ((a.peso == b.peso) ? 0 : ((a.peso > b.peso) ? 1 : -1 ));
+                             break;
+                  case "fecha_creacion":return ((a.fecha_creac < b.fecha_creac) ? -1 : ((a.fecha_creac > b.fecha_creac) ? 1 : 0 ));
+                             break;
+                  case "ong":return ((a.name == b.name) ? 0 : ((a.name > b.name) ? 1 : -1 ));
+                             break;
                                };
 
                 });
                var viajes= {"data": {
-                            "viajes" : $.map(viajesFiltrados,function(value,index){
+                            "viajes" : $.map(viajesOrdenados,function(value,index){
                                        return value;})
                                     }
                             };
                 navigationController.loadTemplate('listViajes',viajes,'#listadoViajes');
-            });*/
+            });
 
           });
         },"json");
