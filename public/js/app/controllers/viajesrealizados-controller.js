6c94e33f1b524f13207aab5f4a05dce4ef0a3415
fninni <facuninni@gmail.com>
2016-11-22 23:36:33 -0300

    cambios en la actualizacion del dom

--- public/js/app/controllers/viajesrealizados-controller.js
+++ public/js/app/controllers/viajesrealizados-controller.js
@@ -14,42 +14,36 @@ ViajesRealizadosController.prototype = {
       var navigationController = new NavigationController;
       $.get('api/v1/viajes/realizados/'+consulta+"/"+estado,function(data){
         navigationController.loadTemplate('realizados-body',data,'#rbody',function(){
-        });
+          var estado=0;
+          var orden="origen";
+          $('.container').on('change','#dropdown-orden',function(){
+            var controller = new ViajesRealizadosController;
+            //remplazar nul por el valor del boton activo
+            orden=$(this).val();
+            controller.loadstruct(orden,estado);
+          });
+
+          //$("#myid li").click(function() {
+          $('.container').on('click','#todos',function(){
+            //ojo aca no va value si no el valor
+            estado=0;
+            var controller = new ViajesRealizadosController;
+            controller.loadstruct(orden,estado);
+          });
+
+          $('.container').on('click','#asignados',function(){
+            var controller = new ViajesRealizadosController;
+            estado=1;
+            controller.loadstruct(orden,estado);
+          });
+
+          $('.container').on('click','#finalizados',function(){
+            var controller = new ViajesRealizadosController;
+            //var orden=$(this).val();
+            estado=2;
+            controller.loadstruct(orden,estado);
+          });});
       },"json");
     }
 
 }
-
-
-
-$(document).ready(function(){
-  var estado=0;
-  var orden="origen";
-  $('.container').on('change','#dropdown-orden',function(){
-    var controller = new ViajesRealizadosController;
-    //remplazar nul por el valor del boton activo
-    orden=$(this).val();
-    controller.loadstruct(orden,estado);
-  });
-
-  //$("#myid li").click(function() {
-  $('.container').on('click','#todos',function(){
-    //ojo aca no va value si no el valor
-    estado=0;
-    var controller = new ViajesRealizadosController;
-    controller.loadstruct(orden,estado);
-  });
-
-  $('.container').on('click','#asignados',function(){
-    var controller = new ViajesRealizadosController;
-    estado=1;
-    controller.loadstruct(orden,estado);
-  });
-
-  $('.container').on('click','#finalizados',function(){
-    var controller = new ViajesRealizadosController;
-    //var orden=$(this).val();
-    estado=2;
-    controller.loadstruct(orden,estado);
-  });
-});
